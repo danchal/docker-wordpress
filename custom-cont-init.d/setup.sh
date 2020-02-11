f638412ccf85b56fc51dcdb99767137a32359cde
danchal <danchal@users.noreply.github.com>
2020-02-11 13:11:12 +0100

    logging

--- custom-cont-init.d/setup.sh
+++ custom-cont-init.d/setup.sh
@@ -4,3 +4,5 @@
 echo "**** custom script $0 ****"
 echo "replacing site-confs/default"
 cp /config/custom-cont-init.d/etc/site-confs-default /config/nginx/site-confs/default
+
+echo "**** PASSED custom script $0 ****"
