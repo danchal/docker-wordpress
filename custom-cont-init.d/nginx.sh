a7dd08341e97d11ec88d5635b7134ec91dc5ae5d
danchal <danchal@users.noreply.github.com>
2020-02-11 12:20:14 +0100

    refactor

--- custom-cont-init.d/nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "**** custom script $0 ****"
-echo "replacing site-confs/default"
-cp /config/custom-files/site-confs-default /config/nginx/site-confs/default
---
+++ custom-cont-init.d/setup.sh
@@ -0,0 +1,6 @@
+# place this whole folder in container /config
+#!/bin/bash
+
+echo "**** custom script $0 ****"
+echo "replacing site-confs/default"
+cp /config/custom-cont-init.d/etc/site-confs-default /config/nginx/site-confs/default
